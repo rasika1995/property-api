527546ec92b6896b6988d7d281c66d5c8af0c7c0
rasika1995 <akh.rasika.priyadarshani@gmail.com>
2023-09-29 10:21:21 +0530

    update config file

--- src/config/prod.ts
+++ src/config/prod.ts
@@ -10,18 +10,7 @@ const config: Config = {
     host: "localhost",
     port: 5432,
     database: "mortru",
-  },
-  jwt: {
-    secretKey: "your-secret-key",
-  },
-  aws: {
-    ses: {
-      accessKeyId: "access-key-id",
-      secretAccessKey: "secret-access-key",
-      region: "eu-north-1",
-      verifedEmailOrDomain: "test-email",
-    },
-  },
+  }
 };
 
 export default config;
